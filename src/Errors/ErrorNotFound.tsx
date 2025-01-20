984de97991846799eacdbcfbdaacc85ddbd70259
ADDFa <fratamaadha18@gmail.com>
2025-01-20 20:57:48 +0700

    menambahkan animasi error

--- src/Errors/ErrorNotFound.tsx
+++ src/Errors/ErrorNotFound.tsx
@@ -1,5 +1,19 @@
+import errNotFound from "assets/animations/errors/not-found.json"
+import Lottie from "lottie-react"
+import { Col } from "react-bootstrap"
+
 const ErrorNotFound = () => {
-    return <h1 className="text-danger text-center">404 Not Found</h1>
+    return (
+        <Col className="vw-100 vh-100 d-flex vh-100 overflow-hidden bg-space-cadet">
+            <Lottie
+                animationData={errNotFound}
+                loop
+                autoPlay
+                style={{ width: "100%", maxWidth: "600px" }}
+                className="mx-auto"
+            />
+        </Col>
+    )
 }
 
 export default ErrorNotFound
--- src/Errors/UnknownError.tsx
+++ src/Errors/UnknownError.tsx
@@ -1,5 +1,19 @@
+import { Col } from "react-bootstrap"
+import Lottie from "lottie-react"
+import serverError from "assets/animations/errors/server-error.json"
+
 const UnknownError = () => {
-    return <div>UnknownError</div>
+    return (
+        <Col className="vw-100 vh-100 overflow-hidden d-flex align-items-center">
+            <Lottie
+                className="mx-auto"
+                style={{ width: "100%", maxWidth: "800px" }}
+                animationData={serverError}
+                loop
+                autoPlay
+            />
+        </Col>
+    )
 }
 
 export default UnknownError
