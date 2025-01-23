8be4aeaf44aec3902848af3636b1ed4e42358525
rifqi naufal <r.naufal2911@gmail.com>
2025-01-23 22:39:52 +0700

    memperbaiki settingan vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import tsConfgPaths from "vite-tsconfig-paths"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsConfgPaths from "vite-tsconfig-paths";
+import { viteStaticCopy } from "vite-plugin-static-copy";  // Gunakan named import
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tsConfgPaths()]
-})
+  base: '/oocidia/',
+  plugins: [
+    react(),
+    tsConfgPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/img/*',
+          dest: 'assets/'
+        }
+      ]
+    })
+  ]
+});
