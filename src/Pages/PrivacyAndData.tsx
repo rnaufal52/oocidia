7cbfec1274e82b1427ff74b057edb6c610ce9a9b
ADDFa <fratamaadha18@gmail.com>
2025-01-23 20:11:52 +0700

    menambahkan animasi

--- src/Pages/PrivacyAndData.tsx
+++ src/Pages/PrivacyAndData.tsx
@@ -3,43 +3,43 @@ import { Fragment } from "react"
 import privacyData from "assets/data/privacy-data.json"
 import { ListGroup } from "react-bootstrap"
 import { Link } from "react-router-dom"
-import { Helmet } from 'react-helmet-async'
-
+import { Helmet } from "react-helmet-async"
 
 const PrivacyAndData = () => {
     const { "privacy-data": privaciesData } = privacyData
 
     return (
         <>
-        <Helmet>
-            <meta name="robots" content="noindex, nofollow" />
-        </Helmet>
-        <Fragment>
-            <TitleDivider>Privacy and Data</TitleDivider>
+            <Helmet>
+                <meta name="robots" content="noindex, nofollow" />
+            </Helmet>
+            <Fragment>
+                <TitleDivider>Privacy and Data</TitleDivider>
 
-            <ListGroup as="ol" numbered className="mt-4">
-                {privaciesData.map(({ description, header, link }, i) => (
-                    <ListGroup.Item
-                        key={i}
-                        as="li"
-                        className="d-flex mb-5 justify-content-between align-items-start bg-smoky-black border-0 border-bottom text-light fs-4 fw-bold"
-                    >
-                        <div className="ms-2 me-auto">
-                            <div>{header}</div>
-                            <p className="fs-6 fw-normal">
-                                {description}{" "}
-                                {link && (
-                                    <Link to="#" className="text-blue-200">
-                                        {link}
-                                    </Link>
-                                )}
-                            </p>
-                        </div>
-                    </ListGroup.Item>
-                ))}
-            </ListGroup>
-        </Fragment>
-    </>
+                <ListGroup as="ol" numbered className="mt-4">
+                    {privaciesData.map(({ description, header, link }, i) => (
+                        <ListGroup.Item
+                            key={i}
+                            as="li"
+                            className="d-flex mb-5 justify-content-between align-items-start bg-smoky-black border-0 border-bottom text-light fs-4 fw-bold animate__animated animate__fadeInLeft"
+                            style={{ animationDelay: `${i / 5}s` }}
+                        >
+                            <div className="ms-2 me-auto">
+                                <div>{header}</div>
+                                <p className="fs-6 fw-normal">
+                                    {description}{" "}
+                                    {link && (
+                                        <Link to="#" className="text-blue-200">
+                                            {link}
+                                        </Link>
+                                    )}
+                                </p>
+                            </div>
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
+            </Fragment>
+        </>
     )
 }
 
--- src/Pages/TermsOfUse.tsx
+++ src/Pages/TermsOfUse.tsx
@@ -3,60 +3,60 @@ import { Fragment } from "react"
 import { ListGroup } from "react-bootstrap"
 import termOfUse from "assets/data/termofuse.json"
 import { Link } from "react-router-dom"
-import { Helmet } from 'react-helmet-async'
-
+import { Helmet } from "react-helmet-async"
 
 const TermsOfUse = () => {
     const { duedate, termofuse } = termOfUse
 
     return (
         <>
-        <Helmet>
-            <meta name="robots" content="noindex, nofollow" />
-        </Helmet>
-        <Fragment>
-            <TitleDivider>Term of use</TitleDivider>
+            <Helmet>
+                <meta name="robots" content="noindex, nofollow" />
+            </Helmet>
+            <Fragment>
+                <TitleDivider>Term of use</TitleDivider>
 
-            <ListGroup as="ul" className="mt-4">
-                <ListGroup.Item
-                    as="li"
-                    className="d-flex mb-5 justify-content-between align-items-start bg-smoky-black border-0 border-bottom text-light fs-4 fw-bold"
-                >
-                    <div className="ms-2 me-auto">
-                        <div>Effective Date: {duedate}</div>
-                        <p className="fs-6">
-                            Welcome to Oocidia. By accessing and using our
-                            website, products, and services, you agree to comply
-                            with the terms and conditions outlined below. If you
-                            do not agree with these terms, please refrain from
-                            using our services.
-                        </p>
-                    </div>
-                </ListGroup.Item>
-            </ListGroup>
-
-            <ListGroup as="ol" numbered className="mt-4">
-                {termofuse.map(({ description, header, link }, i) => (
+                <ListGroup as="ul" className="mt-4">
                     <ListGroup.Item
-                        key={i}
                         as="li"
-                        className="d-flex mb-5 justify-content-between align-items-start bg-smoky-black border-0 border-bottom text-light fs-4 fw-bold"
+                        className="d-flex mb-5 justify-content-between align-items-start bg-smoky-black border-0 border-bottom text-light fs-4 fw-bold animate__animated animate__fadeInLeft"
                     >
                         <div className="ms-2 me-auto">
-                            <div>{header}</div>
-                            <p className="fs-6 fw-normal">
-                                {description}{" "}
-                                {link && (
-                                    <Link to="#" className="text-blue-200">
-                                        {link}
-                                    </Link>
-                                )}
+                            <div>Effective Date: {duedate}</div>
+                            <p className="fs-6">
+                                Welcome to Oocidia. By accessing and using our
+                                website, products, and services, you agree to
+                                comply with the terms and conditions outlined
+                                below. If you do not agree with these terms,
+                                please refrain from using our services.
                             </p>
                         </div>
                     </ListGroup.Item>
-                ))}
-            </ListGroup>
-        </Fragment>
+                </ListGroup>
+
+                <ListGroup as="ol" numbered className="mt-4">
+                    {termofuse.map(({ description, header, link }, i) => (
+                        <ListGroup.Item
+                            key={i}
+                            as="li"
+                            className="d-flex mb-5 justify-content-between align-items-start bg-smoky-black border-0 border-bottom text-light fs-4 fw-bold animate__animated animate__fadeInLeft"
+                            style={{ animationDelay: `${(i + 1) / 5}s` }}
+                        >
+                            <div className="ms-2 me-auto">
+                                <div>{header}</div>
+                                <p className="fs-6 fw-normal">
+                                    {description}{" "}
+                                    {link && (
+                                        <Link to="#" className="text-blue-200">
+                                            {link}
+                                        </Link>
+                                    )}
+                                </p>
+                            </div>
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
+            </Fragment>
         </>
     )
 }
